e5803647f3f98ec3c36aaebf35cc1d976305b6fa
Etienne Onasch <etienneonasch@web.de>
2017-11-29 21:39:27 +0100

    nameRegex eingeführt

--- src/mainClasses/Helpers.java
+++ src/mainClasses/Helpers.java
@@ -1,13 +1,23 @@
 package mainClasses;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import utils.Constants;
 
 final class Helpers {
 	private Helpers() {}
 	
+	
+	private static Pattern namePattern = Pattern.compile(Constants.nameRegex);
+	
+	
 	public static boolean validateName(String name) {
+		
+		Matcher patternMatcher = namePattern.matcher(name);
 		return name.length() >= Constants.USERNAME_MIN_LEN && 
-				name.length() <= Constants.USERNAME_MAX_LEN; 
+				name.length() <= Constants.USERNAME_MAX_LEN && 
+				patternMatcher.matches(); 
 	}
 	
 	
